a0c71c2a1669b65cc87024d7dc6f8328c6a804b9
Şule <sule.kaptan@bil.omu.edu.tr>
2023-02-12 14:03:00 +0300

    add cta

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,16 @@ import './cta.css'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='cta section-padding'>
+      <div className="cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+
+      <div className="cta-btn">
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
 
